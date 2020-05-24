f92e41e3c5a538b2e033216aedb1656b2267c505
yx <271958091@qq.com>
2020-05-24 17:58:10 +0800

    5.24

---
+++ src/pages/home/home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <div>home</div>
+        <router-link to="/list">列表页</router-link>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Home',
+  data () {
+    return {
+    }
+  }
+}
+</script>
