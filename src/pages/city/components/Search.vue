c105ae677e42fbde3a954de2944a4f36da2efc37
yaoxin <271958091@qq.com>
2020-06-05 15:53:20 +0800

    搜索

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,91 @@
+<template>
+    <div>
+        <div class="search">
+            <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音">
+        </div>
+        <div class="search-content" ref="search" v-show="keyword">
+            <ul>
+                <li class="search-item border-bottom" v-for="item of list" :key="item.id">{{item.name}}</li>
+                <li class="search-item border-bottom" v-show="hasNoData">
+                    没有找到匹配数据
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+<script>
+import Bscroll from 'better-scroll'
+export default {
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((val) => {
+            if (val.spell.indexOf(this.keyword) > -1 || val.name.indexOf(this.keyword) > -1) {
+              result.push(val)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.search)
+  }
+}
+</script>
+<style lang="stylus" scoped>
+ @import '~styles/varibles.styl'
+.search
+  height .72rem
+  padding 0 .1rem
+  background $bgColor
+  .search-input
+    box-sizing border-box
+    width 100%
+    height .62rem
+    padding 0 .1rem
+    line-height .62rem
+    text-align center
+    border-radius .06rem
+    color #666
+.search-content
+  position absolute
+  overflow hidden
+  background #eee
+  z-index 1
+  top 1.58rem
+  left 0
+  right 0
+  bottom 0
+  .search-item
+    line-height .62rem
+    padding-left .2rem
+    background-color #fff
+    color #666
+</style>
